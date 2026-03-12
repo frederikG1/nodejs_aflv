160e09d73c4a0a010fcc7f2ae3f8237310ded773
frederikG1 <kendamaftw123@gmail.com>
2026-03-12 08:30:52 +0100

    variables + first_server

---
+++ 01._Miscellaneous/public/js/first_server.js
@@ -0,0 +1,19 @@
+const express = require ('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/energydrinks/:brand/:size', (req, res) => {
+    console.log(req.params);
+    res.send({ data: `
+        Your ${req.params.brand} is very nice.
+        What size it it ${req.params.size}?
+    `});
+});
+
+app.listen(8080, () => {
+  console.log('Server is running on http://localhost:8080');
+});
